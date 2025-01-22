49b899b8af6f2cffd72e000f3b26c99feb45aa16
shahzadakram786 <alyakram786@gmail.com>
2025-01-22 23:08:41 +0500

    A

---
+++ index.js
@@ -0,0 +1,33 @@
+
+require('dotenv').config
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+
+app.get('/', (req , res)=>{
+    res.send('hello world')
+})
+
+app.get('/twitter', (req , res)=>{
+    res.send('hello world this is twiiter')
+})
+
+app.get('/login', (req , res)=>{
+    res.send('<h1>Please login to access</h1>')
+})
+
+app.get('/youtube', (req , res)=>{
+    res.send('<h2>code with ali</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Exampe app listeing in ${port}`)
+})
+
+
+
+
+
+
